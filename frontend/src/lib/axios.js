7c575288413d988d1bb7e6211bad048b3c5d54f2
Vivek Kumar <vivektact16@gmail.com>
2025-10-08 01:46:58 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1";
+const BASE_URL = "https://nexus-backend-bmt3.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
