076d08e514c159d42141b6e99bdfed1bb7f5ece0
vivektact <vivektact16@gmail.com>
2025-10-08 02:52:54 +0530

    update 10

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://nexus-backend-bmt3.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://nexus-backend-bmt3.onrender.com/api/v1",
   withCredentials: true, // send cookies with the request
 });
 
